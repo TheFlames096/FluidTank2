fc9dfbe14e0e996af375f697f7bd62d550e30875
Kotori316 <Kotori316@gmail.com>
2023-05-06 21:55:56 +0900

    Prepare game test

---
+++ forge/src/test/scala/com/kotori316/fluidtank/forge/SideProxyTest.java
@@ -0,0 +1,23 @@
+package com.kotori316.fluidtank.forge;
+
+import net.minecraft.gametest.framework.GameTest;
+import net.minecraft.gametest.framework.GameTestHelper;
+import net.minecraftforge.gametest.GameTestHolder;
+import net.minecraftforge.gametest.PrefixGameTestTemplate;
+
+import com.kotori316.fluidtank.FluidTankCommon;
+
+import static com.kotori316.testutil.GameTestUtil.EMPTY_STRUCTURE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@GameTestHolder(FluidTankCommon.modId)
+@PrefixGameTestTemplate(value = false)
+class SideProxyTest {
+
+    @GameTest(template = EMPTY_STRUCTURE)
+    void checkProxyClass(GameTestHelper helper) throws ReflectiveOperationException {
+        var clazz = Class.forName("com.kotori316.fluidtank.forge.SideProxy$ServerProxy");
+        assertEquals(clazz, FluidTank.proxy.getClass());
+        helper.succeed();
+    }
+}
