2333ab4c679932cedf6127b176577f12d0373f23
Kotori316 <Kotori316@gmail.com>
2023-09-17 16:50:14 +0900

    Remove unused field

--- common/src/main/scala/com/kotori316/fluidtank/render/ReservoirModel.java
+++ common/src/main/scala/com/kotori316/fluidtank/render/ReservoirModel.java
@@ -16,21 +16,17 @@ import net.minecraft.resources.ResourceLocation;
 
 public final class ReservoirModel extends Model {
     public static final ModelLayerLocation LOCATION = new ModelLayerLocation(new ResourceLocation(FluidTankCommon.modId, "reservoir"), "main");
-    private static final String CONTAINER = "container";
-    private static final String FLUID = "fluid";
     private final ModelPart root;
-    private final ModelPart container;
 
     public ReservoirModel(ModelPart root) {
         super(RenderType::entityCutout);
         this.root = root;
-        this.container = root.getChild(CONTAINER);
     }
 
     public static LayerDefinition createDefinition() {
         MeshDefinition meshDefinition = new MeshDefinition();
         PartDefinition partDefinition = meshDefinition.getRoot();
-        partDefinition.addOrReplaceChild(CONTAINER,
+        partDefinition.addOrReplaceChild("container",
             CubeListBuilder.create().texOffs(0, 0).addBox(2, 0, 0, 12, 16, 1),
             PartPose.ZERO
         );
