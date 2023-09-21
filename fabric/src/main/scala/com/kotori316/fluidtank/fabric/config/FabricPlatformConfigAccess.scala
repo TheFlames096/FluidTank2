afe87f0f280a076e715cea09b406eadbda2a8977
Kotori316 <Kotori316@gmail.com>
2023-09-21 22:16:31 +0900

    Log config when loaded

--- fabric/src/main/scala/com/kotori316/fluidtank/fabric/config/FabricPlatformConfigAccess.scala
+++ fabric/src/main/scala/com/kotori316/fluidtank/fabric/config/FabricPlatformConfigAccess.scala
@@ -2,6 +2,7 @@ package com.kotori316.fluidtank.fabric.config
 
 import cats.data.{Ior, NonEmptyChain}
 import cats.implicits.catsSyntaxFoldableOps0
+import com.google.gson.GsonBuilder
 import com.kotori316.fluidtank.FluidTankCommon
 import com.kotori316.fluidtank.config.FluidTankConfig.{E, cast}
 import com.kotori316.fluidtank.config.{ConfigData, FluidTankConfig, PlatformConfigAccess}
@@ -10,11 +11,14 @@ import net.fabricmc.loader.api.FabricLoader
 class FabricPlatformConfigAccess extends PlatformConfigAccess {
   private final val configData: ConfigData = {
     val config = FluidTankConfig.loadFile(FabricLoader.getInstance().getConfigDir, "fluidtank-common.json")
-    config match {
+    val t = config match {
       case Ior.Right(a) => cast[ConfigData](a)
       case Ior.Both(e, partial) => handleMigration(e, partial)
       case Ior.Left(a: NonEmptyChain[E]) => handleMigration(a, ConfigData.DEFAULT)
     }
+    val configString = new GsonBuilder().disableHtmlEscaping.create.toJson(t.createJson)
+    FluidTankCommon.LOGGER.debug("Config: {}", configString)
+    t
   }
 
   private def handleMigration(e: NonEmptyChain[E], partial: ConfigData): ConfigData = {
--- forge/src/main/scala/com/kotori316/fluidtank/forge/config/ForgePlatformConfigAccess.java
+++ forge/src/main/scala/com/kotori316/fluidtank/forge/config/ForgePlatformConfigAccess.java
@@ -1,5 +1,6 @@
 package com.kotori316.fluidtank.forge.config;
 
+import com.google.gson.GsonBuilder;
 import com.kotori316.fluidtank.FluidTankCommon;
 import com.kotori316.fluidtank.config.ConfigData;
 import com.kotori316.fluidtank.config.PlatformConfigAccess;
@@ -33,8 +34,10 @@ public class ForgePlatformConfigAccess implements PlatformConfigAccess {
         if (cached != null) {
             // ignore changes in initial setup
             FluidTankCommon.LOGGER.debug("Reload FluidTank config {}",
-                    event.getConfig().getFileName());
-            cached = tankConfig.createConfigData();
+                event.getConfig().getFileName());
         }
+        cached = tankConfig.createConfigData();
+        var configString = new GsonBuilder().disableHtmlEscaping().create().toJson(cached.createJson());
+        FluidTankCommon.LOGGER.debug("Config: {}", configString);
     }
 }
