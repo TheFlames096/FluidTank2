2374c9bd421a650c26e43200c3de0f51eb38b0c3
Kotori316 <Kotori316@gmail.com>
2023-10-22 15:18:33 +0900

    Add hacks to load fabric recipes with some stupid mixin mods

--- forge/src/genData/scala/com/kotori316/fluidtank/forge/data/PlatformedCondition.scala
+++ forge/src/genData/scala/com/kotori316/fluidtank/forge/data/PlatformedCondition.scala
@@ -1,8 +1,10 @@
 package com.kotori316.fluidtank.forge.data
 
+import cats.data.Ior
 import com.google.gson.{JsonArray, JsonObject}
+import net.minecraft.resources.ResourceLocation
 import net.minecraftforge.common.crafting.CraftingHelper
-import net.minecraftforge.common.crafting.conditions.{NotCondition, TagEmptyCondition}
+import net.minecraftforge.common.crafting.conditions.{AndCondition, ICondition, NotCondition, TagEmptyCondition}
 
 import scala.util.chaining.scalaUtilChainingOps
 
@@ -16,25 +18,55 @@ object PlatformedCondition {
 
   case class Tag(helper: RecipeIngredientHelper) extends PlatformedCondition {
     override def forgeCondition: Option[JsonObject] = {
-      helper.forgeTagLimit.map(t => new NotCondition(new TagEmptyCondition(t)))
-        .map(CraftingHelper.serialize)
+      // If fabricTagLimit is empty, the recipe uses vanilla item, so it should be always loaded.
+      helper.fabricTagLimit.flatMap { _ =>
+        Ior.fromOptions(helper.forgeTagLimit, helper.fabricTagLimit)
+          .map { i =>
+            i.bimap[ICondition, ICondition](l => new TagEmptyCondition(l), r => new TagEmptyCondition(r))
+              .mergeWith((l, r) => new AndCondition(l, r))
+          }.map(t => new NotCondition(t))
+          .map(CraftingHelper.serialize)
+      }
     }
 
     override def fabricCondition: Option[JsonObject] = {
       /*
       Create this
       {
-        "condition": "fabric:item_tags_populated",
+        "condition": "fabric:or",
         "values": [
-          "c:bronze_ingots"
+          {
+            "condition": "fabric:item_tags_populated",
+            "values": [
+              "c:bronze_ingots"
+            ]
+          },
+          {
+            "condition": "fabric:item_tags_populated",
+            "values": [
+              "forge:ingots/bronze"
+            ]
+          }
         ]
       }
       */
-      helper.fabricTagLimit.map { t =>
+      val fabricTagConditionCreator = (t: ResourceLocation) => {
         val o = new JsonObject
         o.addProperty("condition", "fabric:item_tags_populated")
         o.add("values", new JsonArray().tap(_.add(t.toString)))
         o
+      }
+      helper.fabricTagLimit.flatMap { _ =>
+        Ior.fromOptions(helper.fabricTagLimit, helper.forgeTagLimit)
+          .map { i =>
+            i.bimap(fabricTagConditionCreator, fabricTagConditionCreator)
+              .mergeWith { (c1, c2) =>
+                val o = new JsonObject
+                o.addProperty("condition", "fabric:or")
+                o.add("values", new JsonArray().tap(_.add(c1)).tap(_.add(c2)))
+                o
+              }
+          }
       }
     }
   }
