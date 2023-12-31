17766fd54070cbadaa53952a04fbd97b50115623
Kotori316 <Kotori316@gmail.com>
2023-12-31 22:27:57 +0900

    Add test to check drops

--- fabric/src/test/scala/com/kotori316/fluidtank/fabric/gametest/TankPlacementTest.scala
+++ fabric/src/test/scala/com/kotori316/fluidtank/fabric/gametest/TankPlacementTest.scala
@@ -1,11 +1,17 @@
 package com.kotori316.fluidtank.fabric.gametest
 
 import com.kotori316.fluidtank.FluidTankCommon
+import com.kotori316.fluidtank.contents.GenericUnit
+import com.kotori316.fluidtank.fabric.FluidTank
+import com.kotori316.fluidtank.fabric.tank.TankFluidItemHandler
+import com.kotori316.fluidtank.fluids.{FluidAmount, FluidAmountUtil}
 import com.kotori316.fluidtank.tank.Tier
 import net.fabricmc.fabric.api.gametest.v1.FabricGameTest
 import net.minecraft.core.BlockPos
 import net.minecraft.gametest.framework.{GameTestGenerator, GameTestHelper, TestFunction}
+import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.block.{Block, Blocks}
+import org.junit.jupiter.api.Assertions.{assertAll, assertEquals}
 
 import java.util.Locale
 import scala.jdk.javaapi.CollectionConverters
@@ -32,5 +38,59 @@ final class TankPlacementTest extends FabricGameTest {
       .thenExecute(() => helper.assertBlockPresent(fluid, pos.west.north))
       .thenExecute(() => helper.assertBlockPresent(TankTest.getBlock(tier), pos))
       .thenSucceed()
+  }
+
+  @GameTestGenerator
+  def tankDrop(): java.util.List[TestFunction] = {
+    val tests = for {
+      t <- Seq(Tier.WOOD, Tier.STONE, Tier.STAR)
+      f <- Seq(FluidAmountUtil.BUCKET_WATER, FluidAmountUtil.BUCKET_LAVA)
+      amount <- Seq(GenericUnit.ONE_BUCKET, GenericUnit.ONE_BOTTLE, GenericUnit.fromForge(2000))
+      dropName = s"tank_drop_${t}_${f.content.getKey.getPath}_${amount.asForge}".toLowerCase(Locale.ROOT)
+      cloneName = s"tank_clone_${t}_${f.content.getKey.getPath}_${amount.asForge}".toLowerCase(Locale.ROOT)
+
+      test <- Seq(
+        GameTestUtil.create(FluidTankCommon.modId, BATCH_NAME, dropName, g => testGetTankDrop(g, t, f.setAmount(amount))),
+        GameTestUtil.create(FluidTankCommon.modId, BATCH_NAME, cloneName, g => testGetTankClone(g, t, f.setAmount(amount))),
+      )
+    } yield test
+
+    CollectionConverters.asJava(tests)
+  }
+
+  private def testGetTankDrop(helper: GameTestHelper, tier: Tier, fillContent: FluidAmount): Unit = {
+    val pos = BlockPos.ZERO.above()
+    val tankTile = TankTest.placeTank(helper, pos, tier)
+    tankTile.getConnection.getHandler.fill(fillContent, execute = true)
+
+    val drops = Block.getDrops(helper.getBlockState(pos), helper.getLevel, helper.absolutePos(pos), tankTile, helper.makeMockPlayer(), ItemStack.EMPTY)
+    assertEquals(1, drops.size, "Drop was " + drops)
+
+    val stack = drops.get(0)
+    val handler = new TankFluidItemHandler(tier, stack)
+    assertAll(
+      () => assertEquals(FluidTank.TANK_MAP.get(tier).itemBlock, stack.getItem),
+      () => assertEquals(1, stack.getCount),
+      () => assertEquals(fillContent, handler.getTank.content),
+    )
+    helper.succeed()
+  }
+
+  private def testGetTankClone(helper: GameTestHelper, tier: Tier, fillContent: FluidAmount): Unit = {
+    val pos = BlockPos.ZERO.above()
+    val tankTile = TankTest.placeTank(helper, pos, tier)
+    tankTile.getConnection.getHandler.fill(fillContent, execute = true)
+
+    val state = helper.getBlockState(pos)
+    val stack = state.getBlock.getCloneItemStack(
+      helper.getLevel, helper.absolutePos(pos), state
+    )
+    val handler = new TankFluidItemHandler(tier, stack)
+    assertAll(
+      () => assertEquals(FluidTank.TANK_MAP.get(tier).itemBlock, stack.getItem),
+      () => assertEquals(1, stack.getCount),
+      () => assertEquals(fillContent, handler.getTank.content),
+    )
+    helper.succeed()
   }
 }
--- neoforge/src/gameTest/scala/com/kotori316/fluidtank/neoforge/gametest/TankPlacementTest.scala
+++ neoforge/src/gameTest/scala/com/kotori316/fluidtank/neoforge/gametest/TankPlacementTest.scala
@@ -1,12 +1,19 @@
 package com.kotori316.fluidtank.neoforge.gametest
 
 import com.kotori316.fluidtank.FluidTankCommon
+import com.kotori316.fluidtank.contents.GenericUnit
+import com.kotori316.fluidtank.fluids.{FluidAmount, FluidAmountUtil}
+import com.kotori316.fluidtank.neoforge.FluidTank
+import com.kotori316.fluidtank.neoforge.tank.TankFluidItemHandler
 import com.kotori316.fluidtank.tank.Tier
 import com.kotori316.testutil.GameTestUtil
-import net.minecraft.core.BlockPos
+import net.minecraft.core.{BlockPos, Direction}
 import net.minecraft.gametest.framework.{GameTestGenerator, GameTestHelper, TestFunction}
+import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.block.{Block, Blocks}
+import net.minecraft.world.phys.{BlockHitResult, Vec3}
 import net.neoforged.neoforge.gametest.GameTestHolder
+import org.junit.jupiter.api.Assertions.{assertAll, assertEquals}
 
 import java.util.Locale
 import scala.jdk.javaapi.CollectionConverters
@@ -34,5 +41,58 @@ final class TankPlacementTest {
       .thenExecute(() => helper.assertBlockPresent(fluid, pos.west.north))
       .thenExecute(() => helper.assertBlockPresent(TankTest.getBlock(tier).get, pos))
       .thenSucceed()
+  }
+
+  @GameTestGenerator
+  def tankDrop(): java.util.List[TestFunction] = {
+    val tests = for {
+      t <- Seq(Tier.WOOD, Tier.STONE, Tier.STAR)
+      f <- Seq(FluidAmountUtil.BUCKET_WATER, FluidAmountUtil.BUCKET_LAVA)
+      amount <- Seq(GenericUnit.ONE_BUCKET, GenericUnit.ONE_BOTTLE, GenericUnit.fromForge(2000))
+      dropName = s"tank_drop_${t}_${f.content.getKey.getPath}_${amount.asForge}".toLowerCase(Locale.ROOT)
+      cloneName = s"tank_clone_${t}_${f.content.getKey.getPath}_${amount.asForge}".toLowerCase(Locale.ROOT)
+
+      test <- Seq(
+        GameTestUtil.create(FluidTankCommon.modId, BATCH_NAME, dropName, g => testGetTankDrop(g, t, f.setAmount(amount))),
+        GameTestUtil.create(FluidTankCommon.modId, BATCH_NAME, cloneName, g => testGetTankClone(g, t, f.setAmount(amount))),
+      )
+    } yield test
+
+    CollectionConverters.asJava(tests)
+  }
+
+  private def testGetTankDrop(helper: GameTestHelper, tier: Tier, fillContent: FluidAmount): Unit = {
+    val pos = BlockPos.ZERO.above()
+    val tankTile = TankTest.placeTank(helper, pos, tier)
+    tankTile.getConnection.getHandler.fill(fillContent, execute = true)
+
+    val drops = Block.getDrops(helper.getBlockState(pos), helper.getLevel, helper.absolutePos(pos), tankTile, helper.makeMockPlayer(), ItemStack.EMPTY)
+    assertEquals(1, drops.size, "Drop was " + drops)
+
+    val stack = drops.get(0)
+    val handler = new TankFluidItemHandler(tier, stack)
+    assertAll(
+      () => assertEquals(FluidTank.TANK_MAP.get(tier).get().itemBlock, stack.getItem),
+      () => assertEquals(1, stack.getCount),
+      () => assertEquals(fillContent, handler.getTank.content),
+    )
+    helper.succeed()
+  }
+
+  private def testGetTankClone(helper: GameTestHelper, tier: Tier, fillContent: FluidAmount): Unit = {
+    val pos = BlockPos.ZERO.above()
+    val tankTile = TankTest.placeTank(helper, pos, tier)
+    tankTile.getConnection.getHandler.fill(fillContent, execute = true)
+
+    val stack = helper.getBlockState(pos).getCloneItemStack(
+      new BlockHitResult(Vec3.atCenterOf(helper.absolutePos(pos)), Direction.UP, helper.absolutePos(pos), true), helper.getLevel, helper.absolutePos(pos), helper.makeMockPlayer()
+    )
+    val handler = new TankFluidItemHandler(tier, stack)
+    assertAll(
+      () => assertEquals(FluidTank.TANK_MAP.get(tier).get().itemBlock, stack.getItem),
+      () => assertEquals(1, stack.getCount),
+      () => assertEquals(fillContent, handler.getTank.content),
+    )
+    helper.succeed()
   }
 }
