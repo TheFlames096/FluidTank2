708d7b6ce5e5c916002ed1cb838b3c2503b2e2e8
Kotori316 <Kotori316@gmail.com>
2024-01-26 22:55:35 +0900

    Replace to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    alias(libs.plugins.architectury.plugin)
+    alias(libs.plugins.architectury.loom) apply (false)
+    alias(libs.plugins.github.release)
+}
+
+tasks.named("wrapper", Wrapper::class) {
+    gradleVersion = "8.5"
+    distributionType = Wrapper.DistributionType.BIN
+}
+
+architectury {
+    minecraft = project.property("minecraft_version") as String
+}
+
+githubRelease {
+    owner = "Kotori316"
+    repo = "FluidTank2"
+    token(project.findProperty("githubToken") as? String ?: System.getenv("REPO_TOKEN") ?: "")
+    targetCommitish = "1.20"
+    tagName = "v${project.findProperty("mod_version")}"
+    releaseName = "v${project.findProperty("mod_version")} for ${project.findProperty("minecraft_version")}"
+    body = createChangelog()
+    releaseAssets = files(
+        fileTree(findProject(":forge")?.layout?.buildDirectory?.dir("libs")!!) {
+            include("*.jar")
+        },
+        fileTree(findProject(":fabric")?.layout?.buildDirectory?.dir("libs")!!) {
+            include("*.jar")
+        },
+        fileTree(findProject(":neoforge")?.layout?.buildDirectory?.dir("libs")!!) {
+            include("*.jar")
+        },
+    )
+    dryRun = (System.getenv("RELEASE_DEBUG") ?: "true").toBoolean()
+    overwrite = false
+    allowUploadToExisting = false
+}
+
+fun createChangelog(): String {
+    val base = """
+        # Large Fluid Tank
+        
+        | Dependency | Version |
+        | -- | -- |
+        | Minecraft | ${project.property("minecraft_version")} |
+        | Forge | ${project.property("forge_version")} |
+        | Fabric | ${project.property("fabric_api_version")} |
+        | NeoForge | ${project.property("neoforge_version")} |
+        """.trimIndent()
+    val fromFile = rootProject.file(project.property("changelog_file")!!).readText()
+    val shortFormat = fromFile.split("---", limit = 2)[0]
+    return base + System.lineSeparator() + shortFormat
+}
--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -23,5 +23,5 @@ sourceSets {
 dependencies {
     // We depend on fabric loader here to use the fabric @Environment annotations and get the mixin dependencies
     // Do NOT use other classes from fabric loader
-    modImplementation "net.fabricmc:fabric-loader:${project.property("fabric_loader_version")}"
+    modImplementation("net.fabricmc:fabric-loader:${project.property("fabric_loader_version")}")
 }
